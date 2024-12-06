09c1eaf736110609bd21674eb6b0a753b61b9668
Alan opresnik <alanopk328@gmail.com>
2024-12-05 22:20:10 -0300

    feat/Information Divider

---
+++ src/components/UsInformation/UsInformationDivider.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import React from 'react'
+
+const UsInformationDivider = () => {
+    return (
+        <div className='flex flex-col gap-4 md:grid grid-cols-2 items-center place-items-center'>
+            <div>
+                <p className='text-white text-4xl font-semibold mb-6'>Un poco sobre nosotros</p>
+                <p className='text-white text-lg font-semibold'>Somos una agencia de <span className='text-primary'>Programacion</span> especializada en el desarrollo de <span className='text-primary'>aplicaciones moviles y paginas web</span>, contamos con un equipo de programadores con varios años de experiencia que siempre esta <span className='text-primary'>dispuesto a brindar la mejor atencion </span> y el mejor desempeño para conseguir tu Pagina web soñada</p>
+            </div>
+            <div>
+                <Image src={'/Ilustration.png'} alt='ilustration' width={550} height={400} />
+            </div>
+        </div>
+    )
+}
+
+export default UsInformationDivider
