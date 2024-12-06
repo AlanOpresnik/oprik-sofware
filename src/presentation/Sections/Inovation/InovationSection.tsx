4f94103f00811327febc2ebd4c69d3eebfa7e343
Alan opresnik <alanopk328@gmail.com>
2024-12-05 22:20:33 -0300

    feat/InovationSection

---
+++ src/presentation/Sections/Inovation/InovationSection.tsx
@@ -0,0 +1,12 @@
+import Inovation from '@/components/Inovation/Inovation'
+import React from 'react'
+
+const InovationSection = () => {
+  return (
+    <div>
+        <Inovation/>
+    </div>
+  )
+}
+
+export default InovationSection
