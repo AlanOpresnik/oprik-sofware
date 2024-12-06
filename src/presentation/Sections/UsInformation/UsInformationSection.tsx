58f691786343ecc9006907d385ce65a622847734
Alan opresnik <alanopk328@gmail.com>
2024-12-05 22:20:48 -0300

    feat/Information Section

---
+++ src/presentation/Sections/UsInformation/UsInformationSection.tsx
@@ -0,0 +1,12 @@
+import UsInformationDivider from '@/components/UsInformation/UsInformationDivider'
+import React from 'react'
+
+const UsInformationSection = () => {
+    return (
+        <div>
+            <UsInformationDivider />
+        </div>
+    )
+}
+
+export default UsInformationSection
