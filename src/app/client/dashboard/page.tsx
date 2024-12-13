5c8fa3b10a63034f0b9396f7d18b0a931dc63609
Alan opresnik <alanopk328@gmail.com>
2024-12-12 22:02:14 -0300

    feat/dashboard page

--- src/app/client/dashboard/page.tsx
+++ src/app/client/dashboard/page.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import {getServerSession} from 'next-auth/next'
 import api from '@/api/api'
 import { cookies } from 'next/headers'
+import { Dashboard } from '@/components/dashboard'
+import { Sidebar } from '@/components/sidebar'
 const DashboardPage = async () => {
     const cookieStore = await cookies()
     const session = await getServerSession()
@@ -12,7 +14,9 @@ const DashboardPage = async () => {
    const client = await api.getUserByEmail(session.user.email)
     
   return (
-    <div className='mt-24 text-white'>DashboardPage</div>
+    <div className='mt-12 w-full text-white'>
+      <Dashboard/>
+    </div>
   )
 }
 
