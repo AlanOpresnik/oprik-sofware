cc9c487a55f182d7b641dca7fec6e56800ee3dc3
Alan Opresnik <alanopk328@gmail.com>
2024-12-15 18:53:00 -0300

    fix/refactor

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -1,17 +1,19 @@
+import { Sidebar } from "./sidebar";
+import { Header } from "./header";
+import { ProjectList } from "./project-list";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Alert } from "@mui/material";
+import api from "@/api/api";
+import { getServerSession } from "next-auth";
 
-import { Sidebar } from "./sidebar"
-import { Header } from "./header"
-import { ProjectList } from "./project-list"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Alert } from '@mui/material'
-import api from "@/api/api"
-import { getServerSession } from "next-auth"
-
-export async function Dashboard({ userId }: { userId: string }) {//+
-  const order = await api.getOrderById(userId)
-  //order.data?.client.order.plan_name -> METODO PARA OBTENER EL ORDER 
+export async function Dashboard({ userId }: { userId: string }) {
+  //+
+  const order = await api.getOrderById(userId);
+  //order.data?.client.order.plan_name -> METODO PARA OBTENER EL ORDER
+  const { data: orden } = order;
+  if (!orden) return "no hay orden";
   //:TODO MEJORAR LEGIBILIDAD
-  console.log(order)
+  console.log(order);
   return (
     <div className="flex w-full">
       <div className=" flex w-full">
@@ -24,24 +26,60 @@ export async function Dashboard({ userId }: { userId: string }) {//+
               <CardHeader>
                 <CardTitle>Plan contratado</CardTitle>
               </CardHeader>
-              <CardContent className='relative'>
+              <CardContent className="relative">
                 <div className="">
                   <div className="  ">
                     <div className="relative">
-                      <div className="text-5xl font-bold">Plan <span className='text-primary'>{order.data?.client.order.plan_name}</span></div>
+                      <div className="text-5xl font-bold">
+                        Plan{" "}
+                        <span className="text-primary">
+                          {orden.client.order.plan_name}
+                        </span>
+                      </div>
                       <div>
-                        <p className="text-xl font-bold mt-6">Fecha de compra <span className='text-primary'>29/07/2024</span></p>
-                        <p className="text-xl font-bold mt-4">Valor de la compra <span className='text-primary'>$98.900.00</span></p>
-                        <p className="text-xl font-bold mt-4">Compra hecha a nombre de<span className='text-primary'> Julian mercedes</span></p>
-                        <p className="text-xl font-bold mt-4">Metodo de pago<span className='text-primary'> Mercado pago</span></p>
-                        <p className="text-xl font-bold mt-4">Estado del pago<span className='text-primary'> Exitoso</span></p>
-                        <p className="text-xl font-bold mt-4">Tiempo de entrega aproximado <span className='text-primary'>18 dias</span></p>
-                        <p className="text-xl font-bold mt-4">Desarrollador a cargo <span className='text-primary'>Alan Opresnik</span></p>
-                        <p className="text-xl font-bold mt-4">Contacto <span className='text-primary'>+54 11 1111111 </span></p>
-                        <div className='xl:absolute bottom-[-180px]'>
-                          <Alert sx={{
-                            marginTop: 4
-                          }} severity="warning">A medida que el desarrollador avance con tu web podras ir viendola en tiempo real en el cuadrante siguiente</Alert>
+                        <p className="text-xl font-bold mt-6">
+                          Fecha de compra{" "}
+                          <span className="text-primary">29/07/2024</span>
+                        </p>
+                        <p className="text-xl font-bold mt-4">
+                          Valor de la compra{" "}
+                          <span className="text-primary">$98.900.00</span>
+                        </p>
+                        <p className="text-xl font-bold mt-4">
+                          Compra hecha a nombre de
+                          <span className="text-primary"> Julian mercedes</span>
+                        </p>
+                        <p className="text-xl font-bold mt-4">
+                          Metodo de pago
+                          <span className="text-primary"> Mercado pago</span>
+                        </p>
+                        <p className="text-xl font-bold mt-4">
+                          Estado del pago
+                          <span className="text-primary"> Exitoso</span>
+                        </p>
+                        <p className="text-xl font-bold mt-4">
+                          Tiempo de entrega aproximado{" "}
+                          <span className="text-primary">18 dias</span>
+                        </p>
+                        <p className="text-xl font-bold mt-4">
+                          Desarrollador a cargo{" "}
+                          <span className="text-primary">Alan Opresnik</span>
+                        </p>
+                        <p className="text-xl font-bold mt-4">
+                          Contacto{" "}
+                          <span className="text-primary">+54 11 1111111 </span>
+                        </p>
+                        <div className="xl:absolute bottom-[-180px]">
+                          <Alert
+                            sx={{
+                              marginTop: 4,
+                            }}
+                            severity="warning"
+                          >
+                            A medida que el desarrollador avance con tu web
+                            podras ir viendola en tiempo real en el cuadrante
+                            siguiente
+                          </Alert>
                         </div>
                       </div>
                     </div>
@@ -90,6 +128,5 @@ export async function Dashboard({ userId }: { userId: string }) {//+
         </main>
       </div>
     </div>
-  )
+  );
 }
-
